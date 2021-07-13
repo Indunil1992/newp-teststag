eb1f1a3764ad52179bfbd2eb80d8c13ab6a35e22
Indunil1992 <indunil@adroitlogic.com>
2021-07-13 12:33:16 +0530

    commit to new B 1

---
+++ newFF10.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+
+exports.handler = async (event) => {
+    try {
+        let data = await sqs.receiveMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_DEFAULT_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/indunil-message-queue`,
+            MaxNumberOfMessages: 1,
+            VisibilityTimeout: 30,
+            WaitTimeSeconds: 0,
+            AttributeNames: ['All']
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
+};
